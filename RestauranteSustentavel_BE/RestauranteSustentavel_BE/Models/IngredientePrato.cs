28539cf3bf34fd6656572dfac3fc760465eefcec
Thais Kurmann <thais.martin.ferraz@gmail.com>
2023-05-22 18:22:10 +0200

    Criada classe IngredientePrato

---
+++ RestauranteSustentavel_BE/RestauranteSustentavel_BE/Models/IngredientePrato.cs
@@ -0,0 +1,10 @@
+﻿namespace RestauranteSustentavel_BE.Models
+{
+    public class IngredientePrato
+    {
+        public int idIngrediente { get; set; }
+        public int idPrato { get; set; }
+        public int quantidade { get; set; }
+
+    }
+}
